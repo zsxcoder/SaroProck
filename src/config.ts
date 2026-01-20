ec6d0cbc0dc9c1968c0d1a1031ca12c1868a345e
mcyzsx <mcyzsx@outlook.com>
2026-01-20 13:53:35 +0800

    refactor(config): 更新管理员和敏感用户配置信息
    将管理员信息和敏感用户列表更新为新的博主数据，包括昵称、邮箱、网站和头像链接

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
 // src/config.ts
 // 针对博主特定的数据
 export const ADMIN_USER = {
-  nickname: "EveSunMaple",
-  email: "evesunmaple@outlook.com",
-  website: "https://www.saroprock.com",
-  avatar: "https://www.saroprock.com/avatar.webp",
+  nickname: "钟神秀",
+  email: "3149261770@qq.com",
+  website: "https://www.zsxcoder.top",
+  avatar: "https://home.zsxcoder.top/api/avatar.png",
 };
 
 // 你不希望用户使用的名称与邮箱
 export const SENSITIVE_USERS = [
-  "evesunmaple",
-  "EveSunMaple",
-  "sunmaple",
-  "SunMaple",
+  "ZSx",
+  "钟神秀",
+  "Zsx",
+  "ZSX",
   "admin",
   "博主",
-  "evesunmaple@outlook.com",
+  "3149261770@qq.com",
 ];
